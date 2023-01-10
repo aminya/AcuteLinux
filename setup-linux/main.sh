6816635a702e8b93c8391001ce000f1b95dcf3de
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-01-09 18:22:29 -0800

    fix: fix various issues in the install script

--- setup-linux/main.sh
+++ setup-linux/main.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
+set -e
 
 # Basics
 sudo apt update -qq
@@ -17,20 +18,28 @@ curl -LJO "https://github.com/aminya/setup-cpp/releases/download/${setup_cpp_ver
 # Nala
 sudo node ./setup_cpp.js --nala true
 
+# Upgrade
+sudo nala upgrade -y
+
 # C++, Python, Brew
-sudo node ./setup_cpp.js --compiler llvm --cmake true --ninja true --cppcheck true --ccache true --vcpkg true --doxygen true --gcovr true --task true --sccache true --python true --brew true --make true --bazel true
+sudo node ./setup_cpp.js --compiler llvm --cmake true --ninja true --cppcheck true --ccache true --vcpkg true --doxygen true --gcovr true --task true --python true --make true --bazel true
+sudo chmod a+w "$HOME/.cpprc"
+node ./setup_cpp.js --brew true --sccache true
+
+source "$HOME/.cpprc"
 
 # Rust
-export RUSTUP_HOME=/usr/local/rustup
-export CARGO_HOME=/usr/local/cargo
-export PATH=/usr/local/cargo/bin:$PATH
+export RUSTUP_HOME="$HOME/rustup"
+export CARGO_HOME="$HOME/cargo"
+export PATH="$HOME/cargo/bin:$PATH"
 
 rust_arch='x86_64-unknown-linux-gnu'
 curl -LJO "https://static.rust-lang.org/rustup/dist/${rust_arch}/rustup-init"
 
 chmod +x rustup-init
 ./rustup-init -y --no-modify-path --default-toolchain none
-chmod -R a+w $RUSTUP_HOME $CARGO_HOME
+source "$HOME/.cargo/env"
+sudo chmod -R a+w $RUSTUP_HOME $CARGO_HOME
 
 rust_toolchain_version="nightly-2022-10-17"
 rustup install $rust_toolchain_version
@@ -43,11 +52,11 @@ sudo nala install -y --no-install-recommends pkg-config libssl-dev git
 sudo nala install -y python3-venv python3-pip python3-cachecontrol python3-poetry
 python3 -m pip install pylint mypy perflint -U
 
-# Upgrade
+# Re-Upgrade
 sudo nala upgrade -y
 
 # Clean up
-sudo nala autoremove
-sudo nala autopurge
+sudo nala autoremove -y
+sudo nala autopurge -y
 sudo nala clean --lists
 sudo rm -rf /tmp/*
