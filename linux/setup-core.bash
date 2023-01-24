f5c101b7061eb6414a829ea9d2a5ee574ed6bc61
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-01-23 19:51:58 -0800

    fix: fix the rust installation

--- linux/setup-core.bash
+++ linux/setup-core.bash
@@ -36,14 +36,14 @@ source "$HOME/.cpprc"
 # Rust
 export RUSTUP_HOME="$HOME/rustup"
 export CARGO_HOME="$HOME/cargo"
-export PATH="$HOME/cargo/bin:$PATH"
+export PATH="$CARGO_HOME/bin:$PATH"
 
 rust_arch='x86_64-unknown-linux-gnu'
 curl -LJO "https://static.rust-lang.org/rustup/dist/${rust_arch}/rustup-init"
 
 chmod +x rustup-init
 ./rustup-init -y --no-modify-path --default-toolchain none
-source "$HOME/.cargo/env"
+source "$CARGO_HOME/env"
 sudo chmod -R a+w "$RUSTUP_HOME" "$CARGO_HOME"
 
 rust_toolchain_version="nightly-2022-10-17"
