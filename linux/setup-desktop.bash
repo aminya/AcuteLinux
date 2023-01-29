657951da8986faaa0ac09a49fb68b02ce371b918
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-01-29 03:31:58 -0800

    feat: add flatpacks, vscode, and edge

--- linux/setup-desktop.bash
+++ linux/setup-desktop.bash
@@ -2,11 +2,35 @@
 # -*- coding: utf-8 -*-
 set -e
 
+source "$HOME/.cpprc"
+
+# Microsoft Edge, VsCode
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo 'deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/edge stable main' | sudo tee /etc/apt/sources.list.d/microsoft-edge.list
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+rm -f packages.microsoft.gpg
+
+sudo nala update
+sudo nala install -y --no-install-recommends apt-transport-https microsoft-edge-stable code
+
+
 # Flatpak for KDE
 sudo nala install -y --no-install-recommends flatpak plasma-discover-backend-flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+# Flatpaks
+flatpak -y install flathub \
+    io.github.shiftey.Desktop \
+    net.blix.BlueMail \
+    md.obsidian.Obsidian \
+    com.github.eneshecan.WhatsAppForLinux \
+    org.telegram.desktop com.slack.Slack \
+    com.github.tchx84.Flatseal \
+    us.zoom.Zoom \
+    com.discordapp.Discord \
+    com.spotify.Client
+
 # GitHub
-source "$HOME/.cpprc"
 brew install gh
 gh auth login
