7dd1067e28aa6d6b0ad35e800cab01b0f1829360
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-09-09 00:05:55 -0700

    fix: clean docker images in cleanup

--- src/clean.bash
+++ src/clean.bash
@@ -6,3 +6,6 @@ sudo nala autoremove -y
 sudo nala autopurge -y
 sudo nala clean --lists
 sudo rm -rf /tmp/*
+
+docker prune
+docker images -a | grep none | awk '{ print $3; }' | xargs docker rmi --force
